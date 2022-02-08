4cfa61f7de2225382f96721d1e00234a2f1d66b7
unknown <sathyasai.vajhala@absyz.com>
2022-02-08 13:23:52 +0530

    latest code

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.listen(process.env.PORT || 3000);
+
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
