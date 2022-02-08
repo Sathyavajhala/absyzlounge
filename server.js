69263f28350f68b8e5052cb4b39152627c322008
unknown <sathyasai.vajhala@absyz.com>
2022-02-08 13:28:07 +0530

    latest code

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-app.listen(process.env.PORT || 3000);
+const express = require('express')
+const path = require('path');
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//   });
+const app = express()
+const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.listen(port, () => console.log(`App is live on port ${port}!`))
